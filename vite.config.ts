45d4b5f2bb5bca96967d609866e23151402da763
LeoDog896 <LeoDog896@hotmail.com>
2023-02-06 13:41:06 +0000

    feat: vite-plugin-wasm-pack integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import wasmPack from 'vite-plugin-wasm-pack';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), wasmPack("./sqlparser-wasm")],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
