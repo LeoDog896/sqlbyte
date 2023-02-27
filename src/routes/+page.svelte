e6dd3ab03623e2db9f8cd524895b2fdd90cbb3fe
Tristan F <LeoDog896@hotmail.com>
2023-02-27 15:36:08 +0000

    feat: formatted outputs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,15 +12,52 @@
     })
 
     async function query(input: string) {
-        result = await db.query(input)
+        try {
+            result = await db.query(input)
+        } catch (e) {
+            result = e;
+        }
+    }
+
+    function formatResult(type: string, data: any): string | any[] | { type: "error", data: any } {
+        switch (type) {
+            case "CREATE TABLE":
+                return "Table created";
+            case "INSERT":
+                return `${data.affected} rows inserted`;
+            case "SELECT":
+                return data.rows;
+            default:
+                return { type: "error", data }
+        }
     }
 
 </script>
 
-<input bind:value={input}>
+<textarea cols=80 rows=20 bind:value={input}></textarea>
 <button on:click={() => {
     query(input);
-    input = '';
 }}>Submit</button>
-
-<p>{JSON.stringify(result)}</p>
+<br/>
+{#if typeof result == "string"}
+    <p>Errr: {result}</p>
+{:else if Array.isArray(result)}
+    {#each result as part}
+        {@const formatted = formatResult(part.type, part)}
+        {#if typeof formatted == "string"}
+            <p>{formatted}</p>
+        {:else if typeof formatted == "object" && formatted.type == "error"}
+            <p>Error: {JSON.stringify(formatted)}</p>
+        {:else if Array.isArray(formatted)}
+            {JSON.stringify(formatted)}
+        {:else}
+            <p>Unexpected type, please report this:</p>
+            {JSON.stringify(formatted)}
+        {/if}
+    {/each}
+{:else if result == undefined}
+    <p><i>No result yet. Run a query!</i></p>
+{:else}
+    <p>Unknown result:</p>
+    <p>{JSON.stringify(result)}</p>
+{/if}
