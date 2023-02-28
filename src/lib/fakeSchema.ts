d0d35619b5c32924a27811b8f402466add72d7fe
Tristan F <LeoDog896@hotmail.com>
2023-02-28 18:37:54 +0000

    feat: relative emails

--- src/lib/fakeSchema.ts
+++ src/lib/fakeSchema.ts
@@ -3,6 +3,10 @@ import { faker } from "@faker-js/faker"
 
 JSONSchemaFaker.extend("faker", () => faker);
 
+JSONSchemaFaker.option({
+    resolveJsonPath: true,
+});
+
 export const fakeSchema = (input: any) => {
     const fakeSchema = JSONSchemaFaker.generate(input);
     return fakeSchema;
